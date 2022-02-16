1c2b23c25e8457aa8035de8791b8935ce0f4b766
Farid Hidayat <farid.hidayat@sagara.id>
2022-02-17 00:06:35 +0700

    feat: restore method

--- app/stores/resources/reducers.ts
+++ app/stores/resources/reducers.ts
@@ -108,3 +108,15 @@ export const hardDelete = createAsyncThunk('resource/hardDelete', async ({ url, 
   return response.data
 })
 
+// Restore Model by ID /collection/{id}/restore
+// Restore Collection by selected Ids /collection/selected/restore --params selected=[1,2,3]
+// Restore all Models in Collection /collection/all/restore
+export const restore = createAsyncThunk('resource/restore', async ({ url, data, params = {}, headers = {}, config = {} }: RequestDataType) => {
+  const token = getCookie('authorization_token')
+  if (token) {
+    headers = { Authorization: `Bearer ${token}`, ...headers }
+  }
+  // const url = `/${collection}/${type}/restore` // Restore of collection
+  const response = await post({ url, data, params, headers, config })
+  return response.data
+})
