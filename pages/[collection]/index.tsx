3a015177ff3eb667b39a8428bdb1fec049105848
Farid Hidayat <farid.hidayat@sagara.id>
2022-02-18 22:11:57 +0700

    feat: collectionpage but not finished yet, it connected to fetch action from resource reducer

--- pages/[collection]/index.tsx
+++ pages/[collection]/index.tsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from 'react'
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 import Head from 'next/head'
 
+import {
+  useAppDispatch,
+  useAppSelector,
+} from '../../app/hooks'
+
+import {
+  fetch,
+} from '../../app/stores/resources'
+
+import {
+  selectResource
+} from '../../app/stores/resources'
+
 const CollectionPage: NextPage = () => {
+  const router = useRouter()
+  const { collection } = router.query
+  const url = `/${collection}`
+
+  const [ params, setParams ] = useState({page: 1, limit: 25})
+
+  const dispatch = useAppDispatch()
+  const {
+    data,
+    response,
+    pending,
+    error,
+  } = useAppSelector(selectResource)
+
+  useEffect(() => {
+    if(!router.isReady) return
+    dispatch(fetch({url}))
+  }, [dispatch, params, url, router.isReady])
+
   return (
     <>
       <Head>
@@ -12,7 +47,7 @@ const CollectionPage: NextPage = () => {
 
       <main>
         <h1>
-          Collection Page
+          Collection Page {pending}
         </h1>
       </main>
     </>
