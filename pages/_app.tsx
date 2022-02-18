8e18956b7df14fa3cd9d98ff4f6507c3b85059d0
Farid Hidayat <farid.hidayat@sagara.id>
2022-02-19 00:04:31 +0700

    feat: checking if page has layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,17 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { store } from '../app/store'
+import { NextPageWithLayout } from '../app/utils/pageTypes'
 
-function MyApp({ Component, pageProps }: AppProps) {
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page)
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      {getLayout(<Component {...pageProps} />)}
     </Provider>
   )
 }
