ae24aa2f4c4d23e1b1c44e34cccedc353cbb11e0
Farid Hidayat <farid.hidayat@sagara.id>
2022-02-18 21:40:09 +0700

    feat: store resource reducers

--- app/store.ts
+++ app/store.ts
@@ -4,10 +4,13 @@ import {
   ThunkAction,
 } from '@reduxjs/toolkit'
 
+import resourceReducer from './stores/resources/reducers'
+
 export const store = configureStore({
   reducer: {
-  // This is where we add reducers.
-  // Since we don't have any yet, leave this empty
+    // This is where we add reducers.
+    // Since we don't have any yet, leave this empty
+    resource: resourceReducer,
   },
 })
 
