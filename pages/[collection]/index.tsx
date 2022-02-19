b0018604ac953cc45242c03f44eee0ff598fbe1a
Farid Hidayat <farid.hidayat@sagara.id>
2022-02-19 15:13:52 +0700

    feat: collection page index

--- pages/[collection]/index.tsx
+++ pages/[collection]/index.tsx
@@ -1,7 +1,15 @@
 import { ReactElement, useEffect, useState } from 'react'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
-import Link from 'next/link'
+
+import AddIcon from '@mui/icons-material/Add'
+import HomeIcon from '@mui/icons-material/Home'
+
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import Link from '@mui/material/Link'
+
 import Head from 'next/head'
 import Layout from '../../app/layouts/layout'
 import { NextPageWithLayout } from '../../app/utils/pageTypes'
@@ -18,6 +26,7 @@ import {
 import {
   selectResource
 } from '../../app/stores/resources'
+import { Box, Breadcrumbs, Button, IconButton, Stack, Typography } from '@mui/material'
 
 const CollectionPage: NextPageWithLayout = () => {
   const router = useRouter()
@@ -39,6 +48,15 @@ const CollectionPage: NextPageWithLayout = () => {
     dispatch(fetch({url}))
   }, [dispatch, params, url, router.isReady])
 
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
     <>
       <Head>
@@ -46,12 +64,58 @@ const CollectionPage: NextPageWithLayout = () => {
         <meta name="description" content="Collection" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>
-        <h1>
-          Collection Page {pending}
-        </h1>
-      </main>
+      <Box sx={{ p: 2, mt: 8, display: 'flex', flexDirection: 'column', }}>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="baseline"
+          spacing={2}
+        >
+          <Breadcrumbs aria-label="breadcrumb">
+            <Link underline="hover" color="inherit" href="/">
+              <HomeIcon />
+            </Link>
+            {/* <Link
+              underline="hover"
+              color="inherit"
+              href="/getting-started/installation/"
+            >
+              Core
+            </Link> */}
+            <Typography color="text.primary">Countries</Typography>
+          </Breadcrumbs>
+          <Stack spacing={2} direction="row">
+            <Button variant="text" startIcon={<AddIcon />}>New</Button>
+            <IconButton
+              aria-label="more"
+              id="long-button"
+              aria-controls={open ? 'long-menu' : undefined}
+              aria-expanded={open ? 'true' : undefined}
+              aria-haspopup="true"
+              onClick={handleClick}
+            >
+              <MoreVertIcon />
+            </IconButton>
+            <Menu
+              id="long-menu"
+              MenuListProps={{
+                'aria-labelledby': 'long-button',
+              }}
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+            >
+              <MenuItem>
+                Quick Add
+              </MenuItem>
+              <MenuItem>
+                <Typography color="error">Trash</Typography>
+              </MenuItem>
+            </Menu>
+          </Stack>
+        </Stack>
+        <Box>Box 2</Box>
+      </Box>
     </>
   )
 }
