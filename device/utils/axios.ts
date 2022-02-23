9a9ef70f273f84a91d9563883d3e14fb51c059f4
Farid Hidayat <farid.hidayat@sagara.id>
2022-02-23 21:54:37 +0700

    chore: APIURL from env variable

--- device/utils/axios.ts
+++ device/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig, AxiosRequestHeaders } from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.APIURL,
+  baseURL: process.env.NEXT_PUBLIC_APIURL,
   timeout: 120 * 1000
 })
 
