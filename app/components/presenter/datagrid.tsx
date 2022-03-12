6376100744e55df1f78c2ca647fd6ab7ef3264bf
Farid Hidayat <farid.hidayat@sagara.id>
2022-03-12 17:40:21 +0700

    feat: datagrid component

--- app/components/presenter/datagrid.tsx
+++ app/components/presenter/datagrid.tsx
@@ -1,8 +1,46 @@
-import { FunctionComponent, useEffect, useState } from "react"
+import { FunctionComponent, useEffect, useState } from 'react'
 import {
   DataGrid,
+  GridApiRef,
+  GridCallbackDetails,
   GridEnrichedColDef,
-  GridPinnedColumns} from '@mui/x-data-grid'
+  GridPinnedColumns,
+  GridRowParams,
+  GridToolbarContainer,
+  MuiEvent
+} from '@mui/x-data-grid'
+import AddIcon from '@mui/icons-material/Add'
+import { Button } from '@mui/material'
+
+interface ToolbarProps {
+  apiRef: GridApiRef;
+}
+
+function DatagridToolbar(props: ToolbarProps) {
+  const { apiRef } = props;
+
+  const handleClick = () => {
+    // const id = randomId()
+    // apiRef.current.updateRows([{ id, isNew: true }])
+    // apiRef.current.setRowMode(id, 'edit')
+    // // Wait for the grid to render with the new row
+    // setTimeout(() => {
+    //   apiRef.current.scrollToIndexes({
+    //     rowIndex: apiRef.current.getRowsCount() - 1,
+    //   })
+    //   apiRef.current.setCellFocus(id, 'name')
+    // })
+  }
+
+  return (
+    <GridToolbarContainer>
+      <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
+        Quick Add
+      </Button>
+    </GridToolbarContainer>
+  )
+}
+
 
 const pinnedColumns: GridPinnedColumns = {
   right: ['actions']
@@ -17,11 +55,13 @@ export type Params = {
 type PropsType = {
   params?: Params
   onRequestData: Function,
+  onRowClick: Function,
   rows: Record<string, any>[] | Record<string, any>,
   columns: GridEnrichedColDef[]
 }
 
-const DatagridPresenter: FunctionComponent<PropsType> = ({ params, onRequestData, rows, columns}: PropsType) => {
+const DatagridPresenter: FunctionComponent<PropsType> = (props: PropsType) => {
+  const { params, onRequestData, onRowClick, rows, columns} = props
   const { page, limit } = params
   const [currentPage, setCurrentPage] = useState<number>(page)
   const [pageSize, setPageSize] = useState<number>(limit)
@@ -47,9 +87,13 @@ const DatagridPresenter: FunctionComponent<PropsType> = ({ params, onRequestData
         pageSize={pageSize}
         rowCount={200}
         onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+        onRowClick={onRowClick}
         rowsPerPageOptions={[5, 10, 25, 50, 100]}
         pagination
         onPageChange={(page) => setCurrentPage(page)}
+        components={{
+          Toolbar: DatagridToolbar,
+        }}
       />
     </div>
   )
