ddf3cdf8e91586786716a5155388e8c3e2d6eca5
Farid Hidayat <farid.hidayat@sagara.id>
2022-03-29 16:02:47 +0700

    feat: default resource repository

---
+++ domain/repositories/resources/index.tsx
@@ -0,0 +1,11 @@
+import data from '@domain/repositories/resources/data'
+import columns from '@domain/repositories/resources/columns'
+import forms from '@domain/repositories/resources/forms'
+import validation from '@domain/repositories/resources/validation'
+
+export {
+  data,
+  columns,
+  forms,
+  validation
+}
