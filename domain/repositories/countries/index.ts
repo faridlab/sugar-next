1488e4fcafaf2b7fe56cd19d5ee8cd4a7a088086
Farid Hidayat <farid.hidayat@sagara.id>
2022-06-21 16:29:33 +0700

    chore: remove columns

--- domain/repositories/countries/index.ts
+++ domain/repositories/countries/index.ts
@@ -1,11 +1,9 @@
 import data from '@domain/repositories/countries/data'
-import columns from '@domain/repositories/countries/columns'
 import forms from '@domain/repositories/countries/forms'
 import validation from '@domain/repositories/countries/validation'
 
 export {
   data,
-  columns,
   forms,
   validation
 }
