6920da75aa234afc307f5c572ee3a15cc30e33eb
Farid Hidayat <farid.hidayat@sagara.id>
2022-06-21 16:30:54 +0700

    feat: registering countries and provinces

--- data/repositories/index.ts
+++ data/repositories/index.ts
@@ -1,7 +1,9 @@
 import * as resources from '@data/repositories/resources'
 import * as countries from '@data/repositories/countries'
+import * as provinces from '@data/repositories/provinces'
 
 export {
   resources,
-  countries
+  countries,
+  provinces
 }
