18bafc9489ada198a071509ce1a31ec2e24f4b26
Farid Hidayat <farid.hidayat@sagara.id>
2022-06-21 14:36:24 +0700

    feat: APIService

--- app/store.ts
+++ app/store.ts
@@ -3,16 +3,30 @@ import {
   configureStore,
   ThunkAction,
 } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
 
 import resourceReducer from './stores/resources'
+import authReducer from './stores/auth'
+
+// SERVICES
+import { apiRequest } from '@service/api/apiRequest'
 
 export const store = configureStore({
   reducer: {
-    // This is where we add reducers.
-    // Since we don't have any yet, leave this empty
     resource: resourceReducer,
+    auth: authReducer,
+
+    [apiRequest.reducerPath]: apiRequest.reducer,
   },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiRequest.middleware),
 })
+
+// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+setupListeners(store.dispatch)
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
