f91a8145fe5a853b9be7477b18e4ec927253c0dd
Farid Hidayat <farid.hidayat@sagara.id>
2022-06-24 01:02:35 +0700

    feat: data from countries domain

--- data/repositories/countries/index.ts
+++ data/repositories/countries/index.ts
@@ -1,10 +1,10 @@
 import {
-  data,
   validation
 } from '@domain/repositories/resources'
 
 import columns from '@data/repositories/countries/columns'
 import forms from '@data/repositories/countries/forms'
+import data from '@data/repositories/countries/data'
 
 const params = {}
 
