80ee343d845a3d62f608046a65e0e489bbe07ae5
Farid Hidayat <farid.hidayat@sagara.id>
2022-06-23 22:56:12 +0700

    feat: forms of countries

---
+++ data/repositories/countries/forms.tsx
@@ -0,0 +1,44 @@
+import { FormLayoutProps } from '@component/forms'
+
+const forms: FormLayoutProps = [
+  [
+    {
+      id: 'name',
+      type: 'TextField',
+      col: 4,
+      label: '',
+      required: false,
+      // used for dynamic props component
+      props: {
+        id: "form-countries-name",
+        label: "Country Name"
+      }
+    }
+  ],
+  [
+    {
+      id: 'isocode',
+      type: 'TextField',
+      col: 2,
+      label: 'ISO Code',
+      required: false,
+      props: {
+        id: "form-countries-isocode",
+        label: "ISO Code"
+      } // used for dynamic props component
+    },
+    {
+      id: 'phonecode',
+      type: 'TextField',
+      col: 2,
+      label: 'Phone Code',
+      required: false,
+      props: {
+        id: "form-countries-phonecode",
+        label: "Phone Code"
+      } // used for dynamic props component
+    }
+  ]
+]
+
+export default forms
