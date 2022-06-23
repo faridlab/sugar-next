71dea5b46582f8b1b8e6c1a2400b73dfcf413aeb
Farid Hidayat <farid.hidayat@sagara.id>
2022-06-24 01:05:07 +0700

    refactor: form component to be controlled component and use context FormContext

--- app/components/forms/components/textField.tsx
+++ app/components/forms/components/textField.tsx
@@ -1,18 +1,26 @@
 import * as React from 'react'
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useContext, useState } from 'react'
 import TextField from '@mui/material/TextField'
-import { FormProps } from '@component/forms'
+import { FormProps, FormContext } from '@component/forms'
 
-const FormTextField: FunctionComponent<FormProps> = ({ props }: FormProps) => {
+const FormTextField: FunctionComponent<FormProps> = (formProps: FormProps) => {
+  const { data, setData } = useContext(FormContext);
+  const { id, props } = formProps
   const properties = {
     fullWidth: true,
     type: 'text',
     ...props,
   }
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setData({ ...data, [id]: event.target.value})
+  };
+
   return (
     <TextField
       {...properties}
+      value={data[id]}
+      onChange={handleChange}
     />
   )
 }
