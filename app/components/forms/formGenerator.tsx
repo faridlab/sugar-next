22532eed45593d4978d7e5b6d196227bbf724ddd
Farid Hidayat <farid.hidayat@sagara.id>
2022-06-23 23:16:22 +0700

    fix: FormGenerator aware about forms props

--- app/components/forms/formGenerator.tsx
+++ app/components/forms/formGenerator.tsx
@@ -1,45 +1,44 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
-import { FormLayoutProps, FormProps, TextField } from '@component/forms'
+import { FormLayoutProps, FormProps } from '@component/forms'
 import { FunctionComponent } from 'react'
+import * as FormComponents from '@component/forms/components'
 
-const forms: FormLayoutProps = [
-  [
-    {
-      id: 'name',
-      type: 'TextField',
-      col: 4,
-      label: '',
-      required: false,
-      props: {} // used for dynamic props component
-    }
-  ]
-]
-
-const FormInputGenerator: FunctionComponent<FormProps> = (props: FormProps) => {
-  return <TextField {...props}  />
+export interface FormGeneratorProps {
+  forms: FormLayoutProps;
 }
 
-export default function FormGenerator() {
+const FormInputGenerator: FunctionComponent<FormProps> = (props: FormProps) => {
+  const { type } = props
+  // FIXME: don't use type any, if it possible find necessary type
+  const FormComponent = (FormComponents as any)[type]
+  return <FormComponent {...props} />
+}
+
+const FormGenerator: FunctionComponent<FormGeneratorProps> = (props: FormGeneratorProps) => {
+  const { forms } = props
   const layout = []
   for (const index in forms) {
     const items = forms[index]
     const fields = []
-    for (const prop of items) {
-      fields.push(<Grid key={`field-${prop.id}`} item xs={4}>
-        <FormInputGenerator {...prop} />
+    for (const props of items) {
+      const { col } = props
+      fields.push(<Grid key={`field-${props.id}`} item xs={col} sx={{mb: 4}}>
+        <FormInputGenerator {...props} />
       </Grid>)
     }
     layout.push(<Grid key={index} container spacing={2}>{fields}</Grid>)
   }
 
   return (
-    <>
-      <Box
-        component="form"
-        sx={{ flexGrow: 1, m: 2 }}
-      >{layout}</Box>
-    </>
+    <Box
+      component="form"
+      sx={{ flexGrow: 1, m: 2 }}
+    >
+      {layout}
+    </Box>
   )
 }
+
+export default FormGenerator
