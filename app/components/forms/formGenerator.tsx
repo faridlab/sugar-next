075f4300a956b9ccac81462ff1de3417ff3d140b
Farid Hidayat <farid.hidayat@sagara.id>
2022-06-24 01:06:59 +0700

    refactor: FormGenerator use Provider<FormContext>

--- app/components/forms/formGenerator.tsx
+++ app/components/forms/formGenerator.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
-import { FormLayoutProps, FormProps } from '@component/forms'
-import { FunctionComponent } from 'react'
+import { FormLayoutProps, FormProps, FormContext } from '@component/forms'
+import { FunctionComponent, useEffect, useState } from 'react'
 import * as FormComponents from '@component/forms/components'
 
 export interface FormGeneratorProps {
   forms: FormLayoutProps;
+  data: Record<string, any>;
 }
+
+type LayoutProps = Pick<FormGeneratorProps, "forms">
 
 const FormInputGenerator: FunctionComponent<FormProps> = (props: FormProps) => {
   const { type } = props
@@ -16,7 +19,7 @@ const FormInputGenerator: FunctionComponent<FormProps> = (props: FormProps) => {
   return <FormComponent {...props} />
 }
 
-const FormGenerator: FunctionComponent<FormGeneratorProps> = (props: FormGeneratorProps) => {
+const FormLayoutGenerator: FunctionComponent<LayoutProps> = (props: LayoutProps) => {
   const { forms } = props
   const layout = []
   for (const index in forms) {
@@ -30,14 +33,26 @@ const FormGenerator: FunctionComponent<FormGeneratorProps> = (props: FormGenerat
     }
     layout.push(<Grid key={index} container spacing={2}>{fields}</Grid>)
   }
+  return (<>{layout}</>)
+}
 
+const FormGenerator: FunctionComponent<FormGeneratorProps> = (props: FormGeneratorProps) => {
+  const { forms } = props
+  const [ data, setData ] = useState(props.data)
+  useEffect(() => {
+    setData(props.data)
+  }, [props.data])
+
+  if (Object.keys(data).length === 0) return <></>
   return (
-    <Box
-      component="form"
-      sx={{ flexGrow: 1, m: 2 }}
-    >
-      {layout}
-    </Box>
+    <FormContext.Provider value={{data, setData}}>
+      <Box
+        component="form"
+        sx={{ flexGrow: 1, m: 2 }}
+      >
+        <FormLayoutGenerator forms={forms} />
+      </Box>
+    </FormContext.Provider>
   )
 }
 
