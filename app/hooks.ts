bc01871afa17ecdf9caab1307c0d8b31234ddbd4
Farid Hidayat <farid.hidayat@sagara.id>
2022-06-24 17:05:38 +0700

    feat: registering dialog hooks

--- app/hooks.ts
+++ app/hooks.ts
@@ -3,6 +3,7 @@ import {
   useDispatch,
   useSelector,
 } from 'react-redux'
+import useDialog from '@app/hooks/dialogs'
 
 import type {
   AppDispatch,
@@ -11,3 +12,7 @@ import type {
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+export {
+  useDialog
+}
---
+++ app/hooks/dialogs.tsx
@@ -0,0 +1,71 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
+import { FunctionComponent, useEffect, useState } from "react";
+
+interface DialogProps {
+  isOpen: boolean;
+  title: string;
+  content: string;
+}
+
+const DialogComponent: FunctionComponent<DialogProps> = (props: DialogProps) => {
+  const [params, setParams] = useState<DialogProps>(props)
+  const { title, content } = params
+
+  useEffect(() => {
+    setParams({...params, ...props})
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props])
+
+  const handleClose = () => {
+    const isOpen: boolean = false
+    setParams({...params, isOpen})
+  }
+
+  return (
+    <Dialog
+      open={params.isOpen}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">
+        {title}
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          {content}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        {/* <Button onClick={handleClose}>Disagree</Button> */}
+        <Button onClick={handleClose} autoFocus>
+          Ok
+        </Button>
+      </DialogActions>
+    </Dialog>)
+}
+
+function useDialog() {
+  const params: DialogProps = {
+    isOpen: false,
+    title: 'Title',
+    content: 'Content'
+  }
+  const [props, setProps] = useState<DialogProps>(params)
+
+  const openDialog = (params: Partial<DialogProps>) => {
+    const isOpen: boolean = true
+    setProps({ ...props, ...params, isOpen })
+  }
+
+  const DialogScreen: FunctionComponent = () => {
+    return (<DialogComponent {...props} />)
+  }
+
+  return {
+    openDialog,
+    DialogScreen
+  }
+}
+
+export default useDialog
