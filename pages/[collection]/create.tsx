2b1d581991f774d94d0da8d6cf863674e3749df5
Farid Hidayat <farid.hidayat@sagara.id>
2022-06-24 17:06:49 +0700

    refactor: onSubmit api and dialog hooks

--- pages/[collection]/create.tsx
+++ pages/[collection]/create.tsx
@@ -5,10 +5,11 @@ import HomeIcon from '@mui/icons-material/Home'
 import Link from '@mui/material/Link'
 
 import * as dataRepositories from '@data/repositories'
+import { usePostMutation } from '@app/services/api/apiRequest'
 
 import Head from 'next/head'
-import Layout from '../../app/layouts/layout'
-import { NextPageWithLayout } from '../../app/utils/pageTypes'
+import Layout from '@app/layouts/layout'
+import { NextPageWithLayout } from '@app/utils/pageTypes'
 
 import FormGenerator from '@component/forms'
 import { FormLayoutProps } from '@component/forms'
@@ -17,6 +18,8 @@ import { Box, Breadcrumbs, Button, Stack, Typography } from '@mui/material'
 
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import { RequestDataType } from '@device/utils/axios'
+import { useDialog } from '@app/hooks'
 
 const CollectionCreatePage: NextPageWithLayout = () => {
   const router = useRouter()
@@ -24,10 +27,10 @@ const CollectionCreatePage: NextPageWithLayout = () => {
   const url = `/${collection}`
   const [ forms, setForms ] = useState<FormLayoutProps>([])
   const [ data, setData ] = useState<Record<string, any>>({})
+  const [ createPost, response ] = usePostMutation()
+  const [ payload, setPayload ] = useState<RequestDataType>({ url, data: {}})
 
-  const onDataChanged = (data: Record<string, any>) => {
-    setData(data)
-  }
+  const { openDialog, DialogScreen} = useDialog()
 
   useEffect(() => {
     if(!router.isReady) return
@@ -36,8 +39,26 @@ const CollectionCreatePage: NextPageWithLayout = () => {
     const data = (dataRepositories as any)[collection as string]?.data || resources.data
     setForms(forms)
     setData(data)
+    setPayload({ ...payload, url: `/${collection}`})
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [ router.isReady ])
+
+  const onSubmit = async () => {
+    try {
+      const {id, ...payloadData} = data;
+      setPayload({
+        ...payload,
+        data: payloadData
+      })
+      const response = await createPost(payload).unwrap()
+    } catch (error) {
+      const { status, message } = (error as any).data
+      openDialog({
+        title: status,
+        content: message
+      })
+    }
+  }
 
   if(!router.isReady) return <>Loading...</>
   return (
@@ -65,7 +86,7 @@ const CollectionCreatePage: NextPageWithLayout = () => {
       <FormGenerator
         forms={forms}
         data={data}
-        onDataChanged={onDataChanged}
+        onDataChanged={setData}
       />
       <AppBar position="fixed" color="inherit" sx={{ top: 'auto', bottom: 0, left: 240, width: 'calc(100vw - 240px)'}}>
         <Toolbar sx={{ display: 'flex', flexDirection: 'row'}}>
@@ -73,10 +94,13 @@ const CollectionCreatePage: NextPageWithLayout = () => {
             <Button href={`/${collection}`}>Cancel</Button>
           </Box>
           <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'flex-end' }}>
-            <Button>Submit</Button>
+            <Button
+              onClick={onSubmit}
+            >Submit</Button>
           </Box>
         </Toolbar>
       </AppBar>
+      <DialogScreen />
     </>
   )
 }
