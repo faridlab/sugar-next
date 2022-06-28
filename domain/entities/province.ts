136f69075fcf19753ea71d380fb262c3d886e60a
Farid Hidayat <farid.hidayat@sagara.id>
2022-06-28 15:57:21 +0700

    fit: fields nullable

--- domain/entities/province.ts
+++ domain/entities/province.ts
@@ -3,8 +3,8 @@ import Country from "@domain/entities/country"
 import City from "@domain/entities/city"
 
 export default interface Province extends Resources {
-  name: string;
-  country_id: number;
+  name: string | null;
+  country_id: number | string | null;
   country?: Country;
   cities?: City[];
 }
