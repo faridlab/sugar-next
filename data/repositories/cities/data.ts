edc5b1f711670c5439b0d2a9327ff4303cf61a0d
Farid Hidayat <farid.hidayat@sagara.id>
2022-07-04 10:18:02 +0700

    fix: city data repository

--- data/repositories/cities/data.ts
+++ data/repositories/cities/data.ts
@@ -1,10 +1,9 @@
 import City from "@domain/entities/city";
 
 const data: Partial<City> = {
-  id: null,
-  name: null,
-  country_id: null,
-  province_id: null,
+  name: '',
+  country_id: '',
+  province_id: '',
 }
 
 export default data
