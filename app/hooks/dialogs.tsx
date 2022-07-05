18c45cd30d28bb112f027d38b850719b79c98511
Farid Hidayat <farid.hidayat@sagara.id>
2022-07-05 19:34:02 +0700

    chore: tidyup line of imports module

--- app/hooks/dialogs.tsx
+++ app/hooks/dialogs.tsx
@@ -1,5 +1,16 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
-import { FunctionComponent, useEffect, useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle
+} from "@mui/material";
+import {
+  FunctionComponent,
+  useEffect,
+  useState
+} from "react";
 
 interface DialogProps {
   isOpen: boolean;
