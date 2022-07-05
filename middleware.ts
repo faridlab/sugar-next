1ac30f694e13802e1ec4a558aabe397c0eb01b10
Farid Hidayat <farid.hidayat@sagara.id>
2022-07-05 19:37:35 +0700

    chore: remove line of unused path

--- middleware.ts
+++ middleware.ts
@@ -141,6 +141,5 @@ export function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  // matcher: '/about/:path*',
   matcher: '/:path*',
 }
