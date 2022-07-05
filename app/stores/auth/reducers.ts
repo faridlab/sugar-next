fa9cf73d5fa4113379ceeacfe65886a93ebdd75d
Farid Hidayat <farid.hidayat@sagara.id>
2022-07-05 19:40:01 +0700

    feat: rename reducer name to be auth, persist value to cookie and checkToken method

--- app/stores/auth/reducers.ts
+++ app/stores/auth/reducers.ts
@@ -1,29 +1,39 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { setCookies, getCookie, checkCookies } from 'cookies-next'
 
 export type InitState = {
-  user: Record<string, any>,
+  user: Record<string, any> | null,
   authorization_token: string | null // NOTE: should null option type will be removed in the future?
 }
 
 const initialState: InitState = {
-  user: {},
+  user: null,
   authorization_token: null
 }
 
 export const authSlice = createSlice({
-  name: 'counter',
+  name: 'auth',
   initialState,
   reducers: {
     setToken: (state, action: PayloadAction<string>) => {
       state.authorization_token = action.payload
+      setCookies('authorization_token', action.payload)
     },
     setUser: (state, action: PayloadAction<Record<string, any>>) => {
       state.user = action.payload
+      setCookies('user', action.payload)
+    },
+    checkToken: (state) => {
+      if(!checkCookies('authorization_token')) return
+      const token = getCookie('authorization_token')
+      const user = getCookie('user')
+      state.authorization_token = (token as string)
+      state.user = JSON.parse((user as string))
     },
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { setUser, setToken } = authSlice.actions
+export const { setUser, setToken, checkToken } = authSlice.actions
 
 export default authSlice.reducer
