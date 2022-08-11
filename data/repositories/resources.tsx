5e1e8231366827a8c63ac8cfaad092c18e03c628
Farid Hidayat <e.faridhidayat@gmail.com>
2022-08-11 15:10:50 +0700

    refactor: default handler actions

--- data/repositories/resources.tsx
+++ data/repositories/resources.tsx
@@ -1,10 +1,4 @@
-import {
-  data,
-  // columns, NOTE: because columns needs type from react-data-grid, it's not possible to use import from domain context
-  forms,
-  validation
-} from '@domain/repositories/resources'
-
+import Router from 'next/router'
 import {
   GridActionsCellItem,
   GridEnrichedColDef,
@@ -14,6 +8,12 @@ import DeleteIcon from '@mui/icons-material/Delete'
 import EditIcon from '@mui/icons-material/Edit'
 import ContentCopyIcon from '@mui/icons-material/ContentCopy'
 import VisibilityIcon from '@mui/icons-material/Visibility'
+import {
+  data,
+  // columns, NOTE: because columns needs type from react-data-grid, it's not possible to use import from domain context
+  forms,
+  validation
+} from '@domain/repositories/resources'
 
 const params = {}
 const columns: GridEnrichedColDef[] = [
@@ -29,28 +29,28 @@ const columns: GridEnrichedColDef[] = [
         icon={<VisibilityIcon />}
         label="Show"
         showInMenu
-        onClick={() => console.log(params)}
+        onClick={() => Router.push(`/${Router.query.collection}/${params.id}`)}
       />,
       <GridActionsCellItem
         key={2}
         icon={<EditIcon />}
         label="Edit"
         showInMenu
-        onClick={() => console.log(params)}
+        onClick={() => Router.push(`/${Router.query.collection}/${params.id}?editable=true`)}
       />,
       <GridActionsCellItem
         key={3}
         icon={<ContentCopyIcon />}
         label="Duplicate"
         showInMenu
-        onClick={() => console.log(params)}
+        onClick={() => Router.push(`/${Router.query.collection}/create?duplicate_from=${params.id}`)}
       />,
       <GridActionsCellItem
         key={4}
         icon={<DeleteIcon />}
         label="Delete"
         showInMenu
-        onClick={() => console.log(params)}
+        onClick={() => alert(`Delete data ${params.id} not implemented yet.`)}
       />,
 
     ],
