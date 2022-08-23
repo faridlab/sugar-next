80d4d34b7100b0686dc657f6491b32f9b740dc4c
Farid Hidayat <e.faridhidayat@gmail.com>
2022-08-24 00:09:30 +0700

    fix: dashboard menu

--- app/layouts/sidemenu.tsx
+++ app/layouts/sidemenu.tsx
@@ -3,62 +3,28 @@ import type { NextPage } from 'next'
 import ListItem from '@mui/material/ListItem'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
-import ListSubheader from '@mui/material/ListSubheader'
-import DashboardIcon from '@mui/icons-material/Dashboard'
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
-import PeopleIcon from '@mui/icons-material/People'
-import BarChartIcon from '@mui/icons-material/BarChart'
-import LayersIcon from '@mui/icons-material/Layers'
-import AssignmentIcon from '@mui/icons-material/Assignment'
+import HomeIcon from '@mui/icons-material/Home'
 import List from '@mui/material/List'
 import Divider from '@mui/material/Divider'
-import { useState } from 'react'
 
 import AdministratorMenu from '@component/layouts/sidebar/administrator'
 import MarketingsMenu from '@component/layouts/sidebar/marketings'
+import PostsMenu from '@component/layouts/sidebar/posts'
 
 const SidemenuComponent: NextPage = () => {
-  const [open, setOpen] = useState(true)
-
-  const handleClick = () => {
-    setOpen(!open);
-  }
-
   return (
     <>
       <Divider />
       <List>
         <ListItem button>
           <ListItemIcon>
-            <DashboardIcon />
+            <HomeIcon />
           </ListItemIcon>
           <ListItemText primary="Dashboard" />
         </ListItem>
-        <ListItem button>
-          <ListItemIcon>
-            <ShoppingCartIcon />
-          </ListItemIcon>
-          <ListItemText primary="Orders" />
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>
-            <PeopleIcon />
-          </ListItemIcon>
-          <ListItemText primary="Customers" />
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>
-            <BarChartIcon />
-          </ListItemIcon>
-          <ListItemText primary="Reports" />
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>
-            <LayersIcon />
-          </ListItemIcon>
-          <ListItemText primary="Integrations" />
-        </ListItem>
       </List>
+      <Divider />
+      <PostsMenu />
       <Divider />
       <MarketingsMenu />
       <Divider />
