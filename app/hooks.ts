26681e70e9028c1745b3bb8a56b0c860f490b2fa
Farid Hidayat <e.faridhidayat@gmail.com>
2022-08-23 22:07:02 +0700

    fix: dialog hooks rename file

--- app/hooks.ts
+++ app/hooks.ts
@@ -3,7 +3,7 @@ import {
   useDispatch,
   useSelector,
 } from 'react-redux'
-import useDialog from '@app/hooks/dialogs'
+import useDialog from '@app/hooks/useDialogs'
 
 import type {
   AppDispatch,
