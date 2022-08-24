24287b1e31fc2ea361f4cd6f574d2e130871800e
Farid Hidayat <e.faridhidayat@gmail.com>
2022-08-25 00:56:32 +0700

    feat: users entity

---
+++ domain/entities/users.ts
@@ -0,0 +1,16 @@
+import Resources from "@domain/entities/resources"
+import { UserStatus } from '@domain/entities/enums'
+import { UUID } from '@domain/entities/types'
+
+export default interface Users extends Resources {
+  id?: UUID;
+
+  first_name: string;
+  last_name: string;
+  username: string;
+  email: string;
+
+  email_verified_at?: string;
+  password?: string;
+  status?: UserStatus;
+}
