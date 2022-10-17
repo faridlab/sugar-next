ae28302991e7508081ca563d876ab0fd171f66f1
Farid Hidayat <e.faridhidayat@gmail.com>
2022-10-18 00:29:27 +0700

    feat(component): FormRating component

---
+++ app/components/forms/components/rating.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import { FunctionComponent, useContext } from 'react'
+import FormControl from '@mui/material/FormControl'
+import FormLabel from '@mui/material/FormLabel'
+import Rating from '@mui/material/Rating'
+import { FormProps, FormContext } from '@component/forms'
+
+const FormRating: FunctionComponent<FormProps> = (formProps: FormProps) => {
+  const { data, setData } = useContext(FormContext)
+  const { id, label, props } = formProps
+  const properties = {
+    fullWidth: true,
+    type: 'text',
+    ...props,
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setData({ ...data, [id]: event.target.value})
+  }
+
+  return (
+    <FormControl component="fieldset">
+      <FormLabel component="legend">{label}</FormLabel>
+      <Rating
+        {...properties}
+        value={data[id]}
+        onChange={handleChange}
+      />
+    </FormControl>
+  )
+}
+
+export default FormRating
