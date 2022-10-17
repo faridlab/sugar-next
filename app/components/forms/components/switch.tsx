31aa4a6b2e178f41f4fa2b02e84335f64bb27cd4
Farid Hidayat <e.faridhidayat@gmail.com>
2022-10-18 00:30:25 +0700

    feat(component): FormSwitch component

---
+++ app/components/forms/components/switch.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react'
+import { FunctionComponent, useContext } from 'react'
+
+import Switch from '@mui/material/Switch'
+import FormGroup from '@mui/material/FormGroup'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import FormControl from '@mui/material/FormControl'
+import FormLabel from '@mui/material/FormLabel'
+
+import { FormProps, FormContext } from '@component/forms'
+
+const FormSwitch: FunctionComponent<FormProps> = (formProps: FormProps) => {
+  const { data, setData } = useContext(FormContext)
+  const { id, label, props } = formProps
+  const properties = {
+    label: props.label,
+    row: true,
+    options: [],
+    ...props,
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let value = data[id] || []
+    if(event.target.checked === true) {
+      value.push(event.target.name)
+    }
+
+    if(event.target.checked === false) {
+      value = value.filter((e: string) => e !== event.target.name)
+    }
+
+    setData({ ...data, [id]: value})
+  }
+
+  const isChecked = (name: string) => {
+    const value = data[id] || []
+    return value.indexOf(name) >= 0
+  }
+
+  const renderOptions = () => {
+    const { options } = properties
+    return options.map((option: Record<string, any>, index) => {
+      return <FormControlLabel
+        key={ index }
+        label={option.label}
+        control={<Switch checked={isChecked(option.value)} onChange={handleChange} name={option.value} />}
+        labelPlacement="end"
+      />
+    })
+  }
+
+  return (
+    <FormControl component="fieldset">
+      <FormLabel component="legend">{label}</FormLabel>
+      <FormGroup aria-label="position" row={properties.row} >
+        {renderOptions()}
+      </FormGroup>
+    </FormControl>
+  )
+}
+
+export default FormSwitch
