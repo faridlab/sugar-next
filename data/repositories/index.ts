0ec398cc208bcc6f950db35682daf5b8747089ca
Farid Hidayat <e.faridhidayat@gmail.com>
2022-10-22 01:08:05 +0700

    refactor(regis): register data repo banner, content and profile

--- data/repositories/index.ts
+++ data/repositories/index.ts
@@ -2,16 +2,23 @@ import * as resources from '@data/repositories/resources'
 import * as countries from '@data/repositories/countries'
 import * as provinces from '@data/repositories/provinces'
 import * as cities from '@data/repositories/cities'
+import * as profile from '@data/repositories/profile'
 import * as users from '@data/repositories/users'
 import * as roles from '@data/repositories/roles'
 import * as permissions from '@data/repositories/permissions'
 
+import * as banners from '@data/repositories/banners'
+import * as contents from '@data/repositories/contents'
+
 export {
   resources,
+  profile,
+  users,
   countries,
   provinces,
   cities,
-  users,
   roles,
   permissions,
+  banners,
+  contents
 }
