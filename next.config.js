b758af715914275b3f99f473c861f3eeb4de54c9
Farid Hidayat <e.faridhidayat@gmail.com>
2023-02-05 22:54:20 +0700

    chore: upgrade next version to v13

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['localhost'],
+const nextConfig = {
+  experimental: {
+    appDir: true,
   },
 }
+
+module.exports = nextConfig
