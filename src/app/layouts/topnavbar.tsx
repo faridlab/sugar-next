c4fafbd62bf057317fc808f06fb7b0ebe5deb8fe
Farid Hidayat <e.faridhidayat@gmail.com>
2023-02-22 23:53:20 +0700

    refactor: remove unused and fix page title

--- src/app/layouts/topnavbar.tsx
+++ src/app/layouts/topnavbar.tsx
@@ -1,62 +1,20 @@
 import type { NextPage } from 'next'
-import { styled, alpha } from '@mui/material/styles'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
-import InputBase from '@mui/material/InputBase'
 import Badge from '@mui/material/Badge'
 import MenuItem from '@mui/material/MenuItem'
 import Menu from '@mui/material/Menu'
 import MenuIcon from '@mui/icons-material/Menu'
-import SearchIcon from '@mui/icons-material/Search'
 import AccountCircle from '@mui/icons-material/AccountCircle'
 import MailIcon from '@mui/icons-material/Mail'
 import NotificationsIcon from '@mui/icons-material/Notifications'
 import MoreIcon from '@mui/icons-material/MoreVert'
-import { useState, MouseEvent, MouseEventHandler } from 'react'
+import { useState, MouseEvent } from 'react'
 import TopbarMenu from '@app/components/layouts/topBarMenu'
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto',
-  },
-}))
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}))
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-}))
+import { Divider } from '@mui/material'
 
 export interface NavProps {
   onToggleDrawer: React.MouseEventHandler<HTMLButtonElement>;
@@ -88,7 +46,6 @@ const TopnavComponent: NextPage<NavProps> = (props: NavProps) => {
   const handleMobileMenuOpen = (event: MouseEvent<HTMLElement>) => {
     setMobileMoreAnchorEl(event.currentTarget)
   }
-  const menuId = 'primary-search-account-menu'
 
   const mobileMenuId = 'primary-search-account-menu-mobile'
   const renderMobileMenu = (
@@ -155,7 +112,7 @@ const TopnavComponent: NextPage<NavProps> = (props: NavProps) => {
         >
           <Box
             sx={{
-              width: 240,
+              width: 255,
               display: 'flex',
               flexDirection: 'row',
               justifyContent: 'flex-start',
@@ -181,7 +138,20 @@ const TopnavComponent: NextPage<NavProps> = (props: NavProps) => {
               Startapp
             </Typography>
           </Box>
-          <Search
+          <Divider orientation="vertical" flexItem />
+          <Box
+            sx={{ml: 2}}
+          >
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ display: { xs: 'none', sm: 'block' }, alignSelf: 'center' }}
+              id="page-title"
+            >
+            </Typography>
+          </Box>
+          {/* <Search
             sx={{ml: '200px'}}
           >
             <SearchIconWrapper>
@@ -191,7 +161,7 @@ const TopnavComponent: NextPage<NavProps> = (props: NavProps) => {
               placeholder="Search…"
               inputProps={{ 'aria-label': 'search' }}
             />
-          </Search>
+          </Search> */}
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
             <IconButton size="large" aria-label="show 4 new mails" color="inherit">
